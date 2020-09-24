fd6798eb3fad27f9929cff986cc615ba58ee9ed9
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2020-09-23 22:19:35 -0700

    playing

---
+++ webpractice/dom.js
@@ -0,0 +1,66 @@
+// Javascript DOM manipulation
+// script for shoplist.html
+/********
+ **Dom Selectors
+ */
+
+var liElems = document.querySelectorAll('li');
+
+// liElems.forEach(function (elem) {
+//   elem.style.background = 'yellow';
+// });
+
+document.querySelector('.second').classList.add('change-font');
+
+var liElem = document.querySelector('li');
+// add css class to an element
+liElem.classList.add('done');
+
+// toggle css class on html elements (turns on or off)
+liElem.classList.toggle('done');
+
+//liElem.classList.toggle('done');
+
+var btn = document.getElementById('enter');
+var input = document.getElementById('enter-item');
+var ul = document.querySelector('ul');
+
+function createListElement () {
+  var li = document.createElement('li');
+  li.textContent = input.value;
+  ul.appendChild(li);
+  input.value = '';
+}
+
+function addOnClick () {
+  if (input.value.length > 0) {
+    createListElement();
+  }
+}
+
+function addOnEnter (e) {
+  if (e.which === 13 && input.value.length > 0) {
+    createListElement();
+  }
+}
+
+btn.addEventListener('click', addOnClick);
+
+input.addEventListener('keypress', addOnEnter);
+
+for (const elem of liElems) {
+  elem.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (e.target.style.background === 'yellow') {
+      e.target.style.background = 'none';
+    } else {
+      e.target.style.background = 'yellow';
+    }
+  });
+};
+
+liElems.addEventListener('click', function (e) {
+  e.preventDefault();
+  e.classList.toggle('color');
+  console.log('poo');
+});
---
+++ webpractice/test.js
@@ -0,0 +1,84 @@
+(function (window) {
+
+  // reformat date function
+  function formatDate(userDate) {
+    // format from M/D/YYYY to YYYYMMDD
+    retStr = userDate.split('/')
+    retStr = retStr[2] + hasZero(retStr[0]) + hasZero(retStr[1])
+    return retStr
+  }
+  console.log(formatDate("1/31/2014"));
+
+  // remove element when clicked on button
+  function setup() {
+    var p = document.getElementsByClassName('remove');
+    console.log(p);
+    for (var i=0;i<p.length;i++) {
+      console.log(i);
+      p[i].addEventListener("click", function (e) {
+        console.log(e.target);
+        e.target.parentElement.remove();
+      });
+    }
+  };
+  setup();
+
+  // remove property in an object if prop is present
+  function removeProperty(obj, prop) {
+    if (prop in obj) {
+      delete obj[prop];
+      return true;
+    }
+    return false;
+  }
+
+  removeProperty(document.getElementById("me"));
+
+  function wrap (execute) {
+    // Return modified function
+    try {
+      return execute.apply(this,arguments);
+    } catch(err) {
+      return 'null';
+    }
+  }
+  
+  var errorExec = wrap(function () {
+    throw new Error('Error');
+  });
+  var resultExec = wrap(function () {
+    return "Result";
+  });
+  
+  console.log(errorExec); // Should output null
+  console.log(resultExec); // Should output "Result"
+
+  function diagonal() {
+    return Math.sqrt(Math.pow(this.width, 2) + Math.pow(this.height, 2));
+  }
+   console.log(diagonal());
+})(window);
+
+// helper function to check single digits
+function hasZero(str) {
+  if (str.length == 1) {
+    return "0" + str;
+  }
+  else {
+    return str
+  }
+}
+
+// for buttons!!
+// toggle on class color
+var btns = document.getElementsByTagName('button');
+
+for (const btn of btns) {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    let divName = e.target.getAttribute('data-btn');
+    let div = document.getElementsByClassName(divName);
+    div[0].classList.toggle('color');
+    console.log('pepe');
+  })
+}
